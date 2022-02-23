fee7ad05a577f95512f1f018bccda82a11a2f991
Jaime Abbariao <jaimeabbariao@gmail.com>
2022-02-22 21:06:43 -0500

    feat: adding utility for testing tree algorithms

---
+++ leetcode/utility/__tests__/tree.test.ts
@@ -0,0 +1,15 @@
+import { assertEquals } from "../../../assert.ts";
+
+import {
+  convertListToTreeNode,
+  inorder
+} from "../tree.ts";
+
+Deno.test("convertListToTreeNode", () => {
+  const items = [1, 2, 3, 4, 5];
+  const result = convertListToTreeNode(items);
+
+  const inorderResult = inorder(result);
+
+  assertEquals(inorderResult, items);
+});
---
+++ leetcode/utility/tree.ts
@@ -0,0 +1,52 @@
+import TreeNode from "../dataStructures/treenode.ts";
+
+export function convertListToTreeNode(items: number[]): TreeNode | null {
+  function helper(items: number[], left: number, right: number): TreeNode | null {
+    if (left >= right) {
+      return null;
+    }
+
+    const mid = Math.floor((right + left) / 2);
+
+    const root = new TreeNode(items[mid]);
+
+    root.left = helper(items, left, mid);
+    root.right = helper(items, mid + 1, right);
+
+    return root;
+  }
+
+  return helper(items, 0, items.length);
+}
+
+export function inorder(root: TreeNode | null): number[] {
+  function traverse(root: TreeNode | null, result: number[]) {
+    if (root) {
+      traverse(root.left, result);
+      result.push(root.val);
+      traverse(root.right, result);
+    }
+  }
+
+  const result: number[] = [];
+
+  traverse(root, result);
+
+  return result;
+}
+
+export function preorder(root: TreeNode | null): number[] {
+  function traverse(root: TreeNode | null, result: number[]) {
+    if (root) {
+      result.push(root.val);
+      traverse(root.left, result);
+      traverse(root.right, result);
+    }
+  }
+
+  const result: number[] = [];
+  
+  traverse(root, result);
+
+  return result;
+}
